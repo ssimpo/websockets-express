5ccb1c612bab6125863014e6479ee7cae9dfbe5a
Stephen <stephen@whitebolt.net>
2017-08-29 11:04:58 +0100

    Pad-out response object
    We now have a basic functioning websocket request/response system that will flow through express routes!

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ const Request = require('./lib/request');
 const Response = require('./lib/response');
 
 function websocketMiddleware(req, res, next) {
-  console.log(req.path, Object.keys(req.headers).sort().join(', ')+'\n\n');
-  if (!req.headers || req.headers.upgrade === undefined || req.headers.upgrade.toLowerCase() !== 'websocket') return next();
+	if (!!req.websocket || !req.headers || req.headers.upgrade === undefined || req.headers.upgrade.toLowerCase() !== 'websocket') {
+		return next();
+	}
 
-  res.websocket((ws, app)=>{
-    ws.on('message', rawData=>{
-      const message = JSON.parse(rawData);
+	res.websocket((client, app)=>{
+		client.on('message', rawData=>{
+			const message = JSON.parse(rawData);
 
-      console.log("Sending websocket message through express", message.path);
-
-      let _req = new Request(req, message);
-      let _res = new Response(_req);
-
-      app.handle(_req, _res);
-    });
-  });
+			if (message.type = 'request') {
+				let _req = new Request(req, message.data);
+				let _res = new Response(_req, client, message.id);
+				app.handle(_req, _res);
+			}
+		});
+	});
 }
 
 function upgrade(server, app) {
-  const wss = new ws.Server({ noServer: true });
-  server.on('upgrade', handleUpgrade(app, wss));
+	const wss = new ws.Server({ noServer: true });
+	server.on('upgrade', handleUpgrade(app, wss));
 }
 
 module.exports = {
-  upgrade, websocketMiddleware
-}
+	upgrade, websocketMiddleware
+};
